aa427e6130b7394f0261dbf526e03823769ebfe8
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-06-22 21:12:59 +0500

    Exceptions

---
+++ src/Exception/AppKernelException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is a part of "comely-io/app-kernel" package.
+ * https://github.com/comely-io/app-kernel
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comely-io/app-kernel/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\App\Exception;
+
+/**
+ * Class AppKernelException
+ * @package Comely\App\Exception
+ */
+class AppKernelException extends \Exception
+{
+}
