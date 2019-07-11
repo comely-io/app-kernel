6e43d157cac64e29f97c84aa60e576805321b482
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-07-11 05:35:39 +0500

    2019.192

--- src/AppKernel.php
+++ src/AppKernel.php
@@ -34,9 +34,9 @@ abstract class AppKernel implements \Serializable
     /** @var string App Name, Extending class should change these constant */
     public const NAME = "Comely App Kernel";
     /** string Comely App Kernel Version (Major.Minor.Release-Suffix) */
-    public const VERSION = "2019.173";
+    public const VERSION = "2019.192";
     /** int Comely App Kernel Version (Major . Minor . Release) */
-    public const VERSION_ID = 201917300;
+    public const VERSION_ID = 201919200;
 
     protected const DIR_CONFIG = null;
     protected const DIR_STORAGE = null;
